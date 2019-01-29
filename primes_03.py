060d8a5af3fb04765ebc590daae4dc408c0c87cd
basilije <basilije@users.noreply.github.com>
2019-01-28 23:53:50 -0800

    Update primes_03.py
    shortening and shortening

--- primes_03.py
+++ primes_03.py
@@ -1,12 +1,10 @@
-numberRange = range(3,1001)
 primes = []
 
-for testNum in numberRange:
-	testRange = range(2, testNum)
-	for divisor in testRange:
+for testNum in range(3,1001):
+	for divisor in range(2, testNum):
 		if testNum % divisor == 0:
 			break
-	if not(testNum % divisor == 0):
+	if not testNum % divisor == 0:
 		primes.append(testNum)
 
 print(primes)
