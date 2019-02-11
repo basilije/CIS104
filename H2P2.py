edea6b9c8e0763ce9cc6791ffaf9e9223446a4aa
basilije <basilije@users.noreply.github.com>
2019-02-11 13:48:48 -0800

    Update H2P2.py

--- H2P2.py
+++ H2P2.py
@@ -2,11 +2,11 @@ import datetime  # We'll need this library for getting a current year
 
 title_of_the_book = input("Enter the title of the book: ")  # Ask an user for a title of the book
 name_of_the_author = input("Enter the name of the author: ")  # Ask an user for a name of the author
-publish_year_ot_the_book = int(input("Enter the publish year of the book: "))  # Ask an user for a publish year of the book
+publish_year_of_the_book = int(input("Enter the publish year of the book: "))  # Ask an user for a publish year of the book
 total_number_of_pages_in_the_book = int(input("Enter the total number of pages in the book: "))  # Ask an user for a total number of pages in the book
 
 current_year = datetime.datetime.today().year  # Get the current year
-age_of_the_book = current_year - publish_year_ot_the_book  # Calculate an age of the book
+age_of_the_book = current_year - publish_year_of_the_book  # Calculate an age of the book
 
 # Print the sentence that depends of age of the book
 if age_of_the_book < 10:
