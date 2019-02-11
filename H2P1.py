2a029f547a62a27b513874a321dd4e148226606f
basilije <basilije@users.noreply.github.com>
2019-02-11 12:38:41 -0800

    Create H2P1.py

---
+++ H2P1.py
@@ -0,0 +1,7 @@
+human_age = input("Enter your age: ")  # Ask user for their age
+human_age_after_ten_years = human_age + 10  # Calculate the age after ten years
+print("In ten years you will be %i years old." % human_age_after_ten_years)  # Print the calculated age
+
+temperature_in_degrees_fahrenheit = input("Enter temperature in degrees Fahrenheit: ")  # Ask user for the temperature in degrees Fahrenheit
+temperature_in_degrees_celsius = (temperature_in_degrees_fahrenheit - 32 ) * 5. / 9.  # Calculate the temperature in degrees Celsius
+print("It is %3.1f in degrees Celsius." % temperature_in_degrees_celsius)  # Print the temperature in degrees Celsius
